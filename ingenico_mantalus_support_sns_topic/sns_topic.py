564f19310499cccb0825a3e7e8f43849929a804c
Sofian So <47451860+sofianso@users.noreply.github.com>
2024-02-07 14:05:51 +1100

    modify to pass account numbers

--- ingenico_mantalus_support_sns_topic/sns_topic.py
+++ ingenico_mantalus_support_sns_topic/sns_topic.py
@@ -4,10 +4,11 @@ from aws_cdk import aws_kms as kms
 from aws_cdk import aws_sns as sns
 from aws_cdk import aws_sns_subscriptions as sns_subscriptions
 from constructs import Construct
+from ingenico_mantalus_support_sns_topic.settings import Settings
 
 
 class MantalusSupportSnsTopicStack(Stack):
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, settings: Settings, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         ###############################
@@ -16,9 +17,20 @@ class MantalusSupportSnsTopicStack(Stack):
 
         # Add any app resource below
 
-        organization_id = "o-a65pr59ao5"  # Ingenico's organization ID
+        sns_kms_key = kms.Key(self, "SNS-KMS-Key", enable_key_rotation=True)
 
-        sns_kms_key = kms.Key(self, "SNS-Key", enable_key_rotation=True)
+        sns_kms_key.add_to_resource_policy(
+            statement=iam.PolicyStatement(
+                actions=["kms:Decrypt", "kms:GenerateDataKey"],
+                principals=[iam.ServicePrincipal("cloudwatch.amazonaws.com")],
+                resources=["*"],
+                conditions={
+                    "StringEquals": {
+                        "AWS:SourceAccount": settings.source_accounts,
+                    }
+                },
+            )
+        )
 
         mantalus_support_sns_topic = sns.Topic(
             self,
@@ -35,7 +47,7 @@ class MantalusSupportSnsTopicStack(Stack):
                 resources=[mantalus_support_sns_topic.topic_arn],
                 conditions={
                     "StringEquals": {
-                        "aws:PrincipalOrgID": organization_id,
+                        "AWS:SourceAccount": settings.source_accounts,
                     }
                 },
             )
